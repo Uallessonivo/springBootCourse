a0f0fb0ead94e105cc447076125da173cdc42a02
Uallessonivo <uallessons@gmail.com>
2023-10-25 15:28:55 -0300

    initial commit

---
+++ src/test/java/com/uallessonivo/springbootcourse/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uallessonivo.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
