d8d5887274d227276844317c98fec376bd99a5ad
Uallessonivo <uallessons@gmail.com>
2023-10-26 15:13:08 -0300

    spring configuration properties

--- src/main/java/com/uallessonivo/springbootcourse/rest/FunRestController.java
+++ src/main/java/com/uallessonivo/springbootcourse/rest/FunRestController.java
@@ -1,10 +1,28 @@
 package com.uallessonivo.springbootcourse.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return teamName;
+    }
+
+    @GetMapping("/coachinfo")
+    public String getCoachName() {
+        return coachName;
+    }
 
     @GetMapping("/")
     public String sayHelloWorld() {
